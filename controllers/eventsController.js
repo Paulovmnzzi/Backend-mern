e6ccdc4f7a429d9e5a2da05d1dd2342f8c275bbe
Paulo Vergara <paulovmnzzi@gmail.com>
2021-11-11 09:44:22 -0300

    testing

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -81,7 +81,7 @@ const updateEvent = async(req, res = response) => {
         console.log(error)
         res.status(500).json({
             ok: false,
-            msg: 'hable con el admin o fijecé'
+            msg: error
         })
     }
 
