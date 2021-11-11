34658bf4f41d57b238df9bd4f0e624c98e260199
Paulo Vergara <paulovmnzzi@gmail.com>
2021-11-11 10:50:44 -0300

    volviendo a como estaba antes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 const cors = require('cors');
-const path = require ('path');
 
 //investigar más sobre esto
 //Crear el servidor de express
@@ -16,7 +15,6 @@ app.use(cors());
 
 //Directorio publico
 app.use(express.static('public'));
-app.use('/login', express.static('public'));
 
 //Lecutra y parseo del body, es decir la información que se mandaría en un input o en el mismo body de postman, por ej
 app.use(express.json()); //middleware
@@ -24,9 +22,6 @@ app.use(express.json()); //middleware
 //Rutas
 app.use('/api/auth', require('./routes/authRoute'));
 app.use('/api/events', require('./routes/eventsRoute'));
-
-
-app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'public', 'index.html')); });
 
 
 
