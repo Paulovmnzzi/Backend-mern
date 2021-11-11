4983f4c1696abb0bf2866a50ff647a62e55fac45
Paulo Vergara <paulovmnzzi@gmail.com>
2021-11-11 10:21:34 -0300

    add solution of comments

--- index.js
+++ index.js
@@ -14,20 +14,23 @@ dbConnection();
 app.use(cors());
 
 //Directorio publico
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 //Lecutra y parseo del body, es decir la información que se mandaría en un input o en el mismo body de postman, por ej
-app.use( express.json() ); //middleware
+app.use(express.json()); //middleware
 
 //Rutas
 app.use('/api/auth', require('./routes/authRoute'));
 app.use('/api/events', require('./routes/eventsRoute'));
 
 
+app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'public', 'index.html')); });
+
+
 
 
 //Escuchar peticiones
 app.listen(process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }` )
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
 
